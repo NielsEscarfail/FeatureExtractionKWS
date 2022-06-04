1339ea6caad66ab720b255ce35ac63d52c7f443c
NielsEscarfail <nescarfail@ethz.ch>
2022-06-04 04:13:46 +0200

    test run multiple mfcc

--- train_ofa_net.py
+++ train_ofa_net.py
@@ -88,7 +88,7 @@ args.manual_seed = 0
 args.lr_schedule_type = "cosine"
 
 args.base_batch_size = 64
-args.valid_size = 1000
+args.valid_size = 10000
 
 args.momentum = 0.9
 args.no_nesterov = False
