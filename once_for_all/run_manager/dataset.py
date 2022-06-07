52b3f56e8d3d5b37de20f8bcb6baa904546694f8
NielsEscarfail <nescarfail@ethz.ch>
2022-06-08 01:23:00 +0200

    test speedup fix

--- once_for_all/run_manager/dataset.py
+++ once_for_all/run_manager/dataset.py
@@ -337,7 +337,7 @@ class AudioProcessor(object):
         sliced_foreground = padded_foreground[time_shift_offset[0]:time_shift_offset[0] + self.desired_samples]
 
         # Mix in background noise
-        background_mul = torch.mul(torch.Tensor(background_noise[:, 0], device=self.device), background_volume)
+        background_mul = torch.mul(torch.tensor(background_noise[:, 0], device=self.device), background_volume)
 
         data = torch.add(background_mul, sliced_foreground)  # Size([16000])
 
