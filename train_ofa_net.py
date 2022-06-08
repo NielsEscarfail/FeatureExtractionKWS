230299e900fe02d1fdd3ec139f6fd4c865fec278
NielsEscarfail <nescarfail@ethz.ch>
2022-06-08 16:01:46 +0200

    Adding MFCC ft_bin params for run1

--- train_ofa_net.py
+++ train_ofa_net.py
@@ -97,7 +97,7 @@ args.manual_seed = 0
 
 args.lr_schedule_type = "cosine"
 
-args.base_batch_size = 64  # 2048 (1.8) 1024 (), 512 (0.5-0.6), 128 and 256 work well, 64 is significantly slower
+args.base_batch_size = 512  # 2048 (1.8) 1024 (), 512 (0.5-0.6), 128 and 256 work well, 64 is significantly slower
 args.valid_size = .1
 
 args.momentum = 0.9
@@ -135,7 +135,7 @@ args.kd_type = "ce"
 args.ft_extr_type = "mfcc"
 # feature_bin_count, spectrogram_length
 # args.ft_extr_params_list = [(10, 20), (10, 30), (10, 49)]
-args.ft_extr_params_list = [(3, 49), (5, 49), (7, 49), (10, 49)]
+args.ft_extr_params_list = [(4, 49), (6, 49), (8, 49), (10, 49), (12, 49), (14, 49), (20, 49)]
 # args.ft_extr_params_list = [(5, 49), (10, 49), (20, 49), (40, 49)]
 # args.ft_extr_params_list = [(10, 10), (20, 10), (40, 10),
 #                            (10, 20), (20, 20), (40, 20),
