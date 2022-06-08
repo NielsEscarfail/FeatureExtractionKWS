4ea707d1a4248ee4fa114586ab344a56a79112e6
NielsEscarfail <nescarfail@ethz.ch>
2022-06-08 16:16:52 +0200

    Change lr

--- once_for_all/run_manager/run_manager.py
+++ once_for_all/run_manager/run_manager.py
@@ -48,7 +48,7 @@ class RunManager:
 
         # net info
         net_info = get_net_info(
-            self.net, self.run_config.data_provider.data_shape, measure_latency, True
+            self.net, self.run_config.data_provider.data_shape, measure_latency, False
         )
         with open("%s/net_info.txt" % self.path, "w") as fout:
             fout.write(json.dumps(net_info, indent=4) + "\n")
