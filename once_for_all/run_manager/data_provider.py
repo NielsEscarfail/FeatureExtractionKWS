271816440315dee07d50032d8bcb15f59d823477
NielsEscarfail <nescarfail@ethz.ch>
2022-06-08 15:37:54 +0200

    fixtest

--- once_for_all/run_manager/data_provider.py
+++ once_for_all/run_manager/data_provider.py
@@ -7,7 +7,7 @@ import torch.utils.data
 from torchaudio.transforms import MFCC
 
 from .dataset import AudioGenerator, AudioProcessor
-torch.multiprocessing.set_start_method('spawn')# good solution !!!!
+# torch.multiprocessing.set_start_method('spawn')  # good solution !!!!
 
 
 class KWSDataProvider:
