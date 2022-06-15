882b1ee03c72629b26ea4bcd9d2c09486be846a6
NielsEscarfail <nescarfail@ethz.ch>
2022-06-15 16:43:18 +0200

    remove warmup

--- train_ofa_net.py
+++ train_ofa_net.py
@@ -46,8 +46,8 @@ if args.task == "normal":
     args.path = "exp/normal"
     args.dynamic_batch_size = 1
     args.n_epochs = 120  # 180 paper
-    args.base_lr = 3e-2  # 1e-3  # 3e-2 - 2.6 paper -> .5-.7?
-    args.warmup_epochs = 5  # 5
+    args.base_lr = 0.001  # 3e-2  # 1e-3  # 3e-2 - 2.6 paper -> .5-.7?
+    args.warmup_epochs = 0  # 5
     args.warmup_lr = -1
     args.ks_list = "7"
     args.expand_list = "6"
