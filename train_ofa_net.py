a19874046c77e5228ba0150173750bc8c2aeddf6
NielsEscarfail <nescarfail@ethz.ch>
2022-06-16 03:24:44 +0200

    melspec setup

--- train_ofa_net.py
+++ train_ofa_net.py
@@ -160,14 +160,12 @@ if args.ft_extr_type == "mel_spectrogram":  # n_mels, win_len
                                 (80, 20), (80, 30), (80, 40), ]
 
 elif args.ft_extr_type == "mfcc":  # n_mfcc/n_mels, win_len
+    args.ft_extr_params_list = [(40, 20), (40, 40), (80, 20), (80, 40)]
+
     """ args.ft_extr_params_list = [(10, 20), (10, 40),
                                 (20, 20), (20, 40),
                                 (30, 20), (30, 40),
                                 (40, 20), (40, 40)]"""
-    args.ft_extr_params_list = [(40, 20), (40, 40),
-                                (60, 20), (60, 40),
-                                (80, 20), (80, 40),
-                                (100, 20), (100, 40)]
 
 elif args.ft_extr_type == "linear_stft":
     args.ft_extr_params_list = [
