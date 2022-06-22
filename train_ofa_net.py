8922ded36aabbdae282681671f2be6d0aa5c49dd
NielsEscarfail <nescarfail@ethz.ch>
2022-06-22 14:00:47 +0200

    reduce epochs

--- train_ofa_net.py
+++ train_ofa_net.py
@@ -49,7 +49,7 @@ args.kd_ratio = 1.0
 if args.task == "normal":
     args.path += "/normal"
     args.dynamic_batch_size = 1
-    args.n_epochs = 80  # 140  # 120  # 180 paper
+    args.n_epochs = 50 # 80  # 140  # 120  # 180 paper
     args.base_lr = 1e-3  # 0.001 # 3e-2  # 0.001  # 3e-2  # 1e-3  # 3e-2 - 2.6 paper -> .5-.7?
     args.warmup_epochs = 5  # 5
     args.warmup_lr = -1
