bb486cb269f9096a4effb9b3d3e0ba2d018a5158
NielsEscarfail <nescarfail@ethz.ch>
2022-06-22 12:39:36 +0200

    Load debug

--- once_for_all/elastic_nn/networks/ofa_kws_net.py
+++ once_for_all/elastic_nn/networks/ofa_kws_net.py
@@ -184,8 +184,8 @@ class OFAKWSNet(KWSNet):
                 raise ValueError(new_key)
             assert new_key in model_dict, "%s" % new_key
             model_dict[new_key] = state_dict[key]
-            print("model dict in ofakwsnet")
-            print(model_dict.keys())
+        print("model dict in ofakwsnet")
+        print(model_dict.keys())
         super(OFAKWSNet, self).load_state_dict(model_dict)
         """if ".mobile_inverted_conv." in key:
             new_key = key.replace(".mobile_inverted_conv.", ".conv.")
