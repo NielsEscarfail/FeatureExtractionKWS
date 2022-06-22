f335909a5db422440dd0b379c7b301d0d16dd4a4
NielsEscarfail <nescarfail@ethz.ch>
2022-06-22 02:02:45 +0200

    fix globalavgpooling

--- once_for_all/networks/kws_net.py
+++ once_for_all/networks/kws_net.py
@@ -13,7 +13,7 @@ class KWSNet(MyNetwork):
 
         self.input_stem = nn.ModuleList(input_stem)
         self.blocks = nn.ModuleList(blocks)
-        self.global_avg_pool = nn.AvgPool2d(kernel_size=(25, 5), stride=1)  # MyGlobalAvgPool2d(keep_dim=True)
+        self.global_avg_pool = nn.AvgPool2d(kernel_size=(26, 5), stride=1)  # MyGlobalAvgPool2d(keep_dim=True)
         self.classifier = classifier
 
     def forward(self, x):
