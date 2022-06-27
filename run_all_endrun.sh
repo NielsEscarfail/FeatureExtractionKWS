d985075ec7d1224672e561f61853a60e10052edd
NielsEscarfail <nescarfail@ethz.ch>
2022-06-27 23:22:56 +0200

    fix typo

--- run_all_endrun.sh
+++ run_all_endrun.sh
@@ -4,3 +4,4 @@ python train_ofa_net.py --task depth --phase 2 --ft_extr_type mfcc 2>&1 | tee te
 python train_ofa_net.py --task depth --phase 3 --ft_extr_type mfcc 2>&1 | tee term_logs/mfcc_kernel2depth3.txt
 python train_ofa_net.py --task expand --phase 1 --ft_extr_type mfcc 2>&1 | tee term_logs/mfcc_depth2width1.txt
 python train_ofa_net.py --task expand --phase 2 --ft_extr_type mfcc 2>&1 | tee term_logs/mfcc_depth2width2.txt
+
