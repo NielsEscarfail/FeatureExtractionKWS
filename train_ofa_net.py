ffc58443a7df16be253e10decfc2097d8c8b972e
NielsEscarfail <nescarfail@ethz.ch>
2022-06-28 16:17:23 +0200

    testval

--- train_ofa_net.py
+++ train_ofa_net.py
@@ -339,8 +339,7 @@ if __name__ == "__main__":
 
     validate_func_dict = {
         "ft_extr_type": args.ft_extr_type,
-        "ft_extr_params_list": [(10, 30), (10, 50),
-                                (40, 30), (40, 50)],
+        "ft_extr_params_list": list(reversed(args.ft_extr_params_list)),
         "width_mult_list": sorted({min(args.width_mult_list), max(args.width_mult_list)}),
         "ks_list": sorted({min(args.ks_list), max(args.ks_list)}),
         "depth_list": sorted({min(net.depth_list), max(net.depth_list)}),
