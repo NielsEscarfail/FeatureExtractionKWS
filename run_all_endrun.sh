5a75b768bab1204b6b0e6e4b9060b90ccf09a6c2
NielsEscarfail <nescarfail@ethz.ch>
2022-06-28 18:29:06 +0200

    fix typo

--- run_all_endrun.sh
+++ run_all_endrun.sh
@@ -1,4 +1,4 @@
-python train_ofa_net.py --ft_extr_type mfcc --task kernel -
+python train_ofa_net.py --ft_extr_type mfcc --task kernel
 python train_ofa_net.py --ft_extr_type mfcc --task depth --phase 1
 python train_ofa_net.py --ft_extr_type mfcc --task depth --phase 2
 python train_ofa_net.py --ft_extr_type mfcc --task depth --phase 3
