3cd0af025defac41f81e0355bd66b47801c74ba8
NielsEscarfail <nescarfail@ethz.ch>
2022-06-28 21:35:09 +0200

    endrun

--- run_all_endrun.sh
+++ run_all_endrun.sh
@@ -1,7 +1,3 @@
-python train_ofa_net.py --ft_extr_type mfcc --task kernel
-python train_ofa_net.py --ft_extr_type mfcc --task depth --phase 1
-python train_ofa_net.py --ft_extr_type mfcc --task depth --phase 2
-python train_ofa_net.py --ft_extr_type mfcc --task depth --phase 3
 python train_ofa_net.py --ft_extr_type mfcc --task expand --phase 1
 python train_ofa_net.py --ft_extr_type mfcc --task expand --phase 2
 python train_ofa_net.py --ft_extr_type mfcc --task width --phase 1
