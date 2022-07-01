b24def85919626797bf5ad4636cb62473fa0ffa6
NielsEscarfail <nescarfail@ethz.ch>
2022-07-01 16:38:10 +0200

    params

--- train_ofa_net.py
+++ train_ofa_net.py
@@ -57,12 +57,12 @@ if args.task == "normal":
     args.path += "/normal"
     args.dynamic_batch_size = 1
     args.n_epochs = 140  # 50  # 80  # 140  # 120  # 180 paper
-    args.base_lr = 1e-3  # 1e-3  # 0.001 # 3e-2  # 0.001  # 3e-2  # 1e-3  # 3e-2 - 2.6 paper -> .5-.7?
+    args.base_lr = 3e-4  # 1e-3  # 0.001 # 3e-2  # 0.001  # 3e-2  # 1e-3  # 3e-2 - 2.6 paper -> .5-.7?
     args.warmup_epochs = 5  # 5
     args.warmup_lr = -1
-    args.ks_list = "3"
-    args.depth_list = "2"
-    args.expand_list = "1"
+    args.ks_list = "7"
+    args.depth_list = "4"
+    args.expand_list = "3"
     args.kd_ratio = 0
 elif args.task == "kernel":
     args.path += "/normal2kernel"
