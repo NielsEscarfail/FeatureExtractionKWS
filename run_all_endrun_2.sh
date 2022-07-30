8cb1fac33f2c00691c2dc0f079e0ae00654a1f20
NielsEscarfail <nescarfail@ethz.ch>
2022-07-30 15:00:23 +0200

    Clean, remove unused

--- run_all_endrun_2.sh
+++
@@ -1,7 +0,0 @@
-echo "ft_extr_type: $1"
-echo "params_id: $2"
-python train_ofa_net.py --task depth --phase 2 --ft_extr_type $1 --params_id $2
-python train_ofa_net.py --task depth --phase 3 --ft_extr_type $1 --params_id $2
-python train_ofa_net.py --task expand --phase 1 --ft_extr_type $1 --params_id $2
-python train_ofa_net.py --task expand --phase 2 --ft_extr_type $1 --params_id $2
-python eval_ofa_net.py --ft_extr_type $1 --params_id $2 --n_arch 1000 --use_json --measure_latency
